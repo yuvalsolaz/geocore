9b1bf8b4910a04123f1920b63b65f73d927ad98c
yuvalsolaz <yuval.solaz@gmail.com>
2020-06-22 15:58:21 +0300

    litshufim

--- src/wordnet.py
+++ src/wordnet.py
@@ -19,14 +19,15 @@ def flatten(lst):
         return [lst]
 
 
-lang = 'arb'
+lang = 'heb'
 
 if __name__ == '__main__':
 
     if len(sys.argv) < 2:
         print(f'usage: python {sys.argv[0]} <word>')
+        exit(1)
 
-    print(f'nltk supported langueges {sorted(wn.langs())}')
+    print(f'nltk supported langueges: \n {sorted(wn.langs())} \n')
 
     eword = sys.argv[1]
     ewords = wn.synsets(eword)
@@ -35,21 +36,18 @@ if __name__ == '__main__':
         exit(1)
 
     print(f'found {len(ewords)} english syns for the word {eword}:')
-    print(' '.join([w.name() for w in ewords]))
-    print()
-    hwords = [wn.synset(ew.name()).lemma_names(lang) for ew in ewords if len(wn.synset(ew.name()).lemma_names(lang)) > 0]
-    hwords_str = '/'.join([' '.join(hw) for hw in hwords])
-    print(f'{lang} synsets for {eword}: {flip_words(hwords_str)}:')
-    print()
-    print(f'hebrew translation: {flip_words(translator.translate(hwords_str,dest="iw").text)}')
-    print()
-    print([ew.definition() for ew in ewords])
-    print()
-    print([ew.lexname() for ew in ewords])
-    print()
-    syns = [[wn.synsets(h,lang=lang) for h in  hw] for hw in hwords]
-    syns_str = ' '.join([l.name() for l in flatten(syns)])
-    print(f'back to english syns:\n {syns_str}')
+
+    for ew in ewords:
+        name = ew.name()
+        lname = wn.synset(ew.name()).lemma_names(lang)
+        trans = flip_words(translator.translate(lname, dest="iw")[0].text if len(lname) > 0 else 'unknown')
+        definition = ew.definition()
+        lex = ew.lexname()
+        syns = [wn.synsets(h, lang=lang) for h in lname]
+        syns_str = ' '.join([l.name() for l in flatten(syns)])
+        print (f'{name} - {lname} - {trans} -  {lex} - {definition} ') # synsets: {syns_str}')
+
+
 
 
 
